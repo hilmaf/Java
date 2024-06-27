bb3d79c7d79c91e2feaf13fcc82d1f4d61edb9c0
gymin <gymin@insunginfo.co.kr>
2024-06-27 17:04:09 +0900

    [정렬] Arrays.sort()

--- sort/src/CollectionSort.java
+++ sort/src/CollectionSort.java
@@ -1,5 +1,6 @@
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,5 +15,9 @@ public class CollectionSort {
 
         Collections.sort(list);
 
+    }
+
+    private void arraySort(int[] arr) {
+        Arrays.sort(arr);
     }
 }
